7c997f955aece2984ccd8c9e3738373808cffdf7
Ayesha <ayeshamohiuddin08@gmail.com>
2026-07-15 23:03:15 +0500

    Updated UI to pro version

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-function getWeather() {
-  const city = document.getElementById("city").value;
+const apiKey = "50b421d4a765aa82039d19a3fdc7092a";
 
-  document.getElementById("result").innerHTML = `
-    <h2>${city}</h2>
-    <p>🌡 Temp: 25°C</p>
-    <p>☁️ Weather: Clear</p>
-  `;
+async function getWeather() {
+  const city = document.getElementById("city").value;
+  const card = document.getElementById("weather-card");
+
+  if (!city) {
+    card.innerHTML = "❌ Please enter city or country";
+    return;
+  }
+
+  card.innerHTML = "⏳ Loading...";
+
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+
+    if (!res.ok) throw new Error();
+
+    const data = await res.json();
+
+    const icon = data.weather[0].icon;
+
+    card.innerHTML = `
+      <h2>${data.name}, ${data.sys.country}</h2>
+      <img src="https://openweathermap.org/img/wn/${icon}@2x.png" />
+
+      <h1>${data.main.temp}°C</h1>
+      <p>${data.weather[0].description}</p>
+
+      <p>🤒 Feels Like: ${data.main.feels_like}°C</p>
+      <p>💧 Humidity: ${data.main.humidity}%</p>
+      <p>🌬 Wind: ${data.wind.speed} m/s</p>
+      <p>🌡 Max: ${data.main.temp_max}°C | Min: ${data.main.temp_min}°C</p>
+    `;
+  } catch (error) {
+    card.innerHTML = "❌ City not found";
+  }
 }
